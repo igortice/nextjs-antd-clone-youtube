956d8247e5b82af444f91c74268a6b35f3faaeda
Igor Rocha <igortice@gmail.com>
2021-02-12 21:55:28 -0300

    init better layout

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,44 +1,83 @@
 import './style.less';
 
-import { Breadcrumb, Layout, Menu } from 'antd';
+import { Layout, Menu } from 'antd';
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons';
 
 import Head from 'next/head';
+import { useState } from 'react';
 
-const { Header, Content, Footer } = Layout;
+const { Header, Sider, Content, Footer } = Layout;
 
-const index = ({ children, title = process.env.NEXT_PUBLIC_NAME_APP }) => (
-  <>
-    <Head>
-      <title>{title}</title>
+const index = ({ children, title = process.env.NEXT_PUBLIC_NAME_APP }) => {
+  const [collapsed, setCollapsed] = useState(true);
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
 
-      <meta
-        name="viewport"
-        content="minimum-scale=1, initial-scale=1, width=device-width"
-      />
-    </Head>
-
-    <Layout className="layout">
-      <Header>
-        <div className="logo" />
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-          <Menu.Item key="1">nav 1</Menu.Item>
-          <Menu.Item key="2">nav 2</Menu.Item>
-          <Menu.Item key="3">nav 3</Menu.Item>
-        </Menu>
-      </Header>
-      <Content style={{ padding: '0 50px' }}>
-        <Breadcrumb style={{ margin: '16px 0' }}>
-          <Breadcrumb.Item>Home</Breadcrumb.Item>
-          <Breadcrumb.Item>List</Breadcrumb.Item>
-          <Breadcrumb.Item>App</Breadcrumb.Item>
-        </Breadcrumb>
-        <div className="site-layout-content">{children}</div>
-      </Content>
-      <Footer style={{ textAlign: 'center' }}>
-        Clone Youtube ©2021 Created by Igor Rocha
-      </Footer>
-    </Layout>
-  </>
-);
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <Layout style={{ height: '100vh' }}>
+        <Sider
+          onBreakpoint={(broken) => setCollapsed(broken)}
+          trigger={null}
+          collapsible
+          breakpoint="lg"
+          collapsed={collapsed}
+        >
+          <div className="logo" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1" icon={<UserOutlined />}>
+              nav 1
+            </Menu.Item>
+            <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+              nav 2
+            </Menu.Item>
+            <Menu.Item key="3" icon={<UploadOutlined />}>
+              nav 3
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Header className="site-layout-background" style={{ padding: 0 }}>
+            {collapsed ? (
+              <MenuUnfoldOutlined
+                className="trigger"
+                onClick={() => setCollapsed(!collapsed)}
+              />
+            ) : (
+              <MenuFoldOutlined
+                className="trigger"
+                onClick={() => setCollapsed(!collapsed)}
+              />
+            )}
+          </Header>
+          <Content
+            className="site-layout-background"
+            style={{
+              margin: '24px 16px',
+              padding: 24,
+              minHeight: 280,
+            }}
+          >
+            {children}
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>
+            Clone YouTube Nextjs/Antd ©2021 Created by Igor Rocha
+          </Footer>
+        </Layout>
+      </Layout>
+    </>
+  );
+};
 
 export default index;
